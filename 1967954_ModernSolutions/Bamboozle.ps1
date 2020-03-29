1d1a53aba293030c343a686bdbb560dc539d3430
Kevin Rumboldt <57267386+Rumk2001@users.noreply.github.com>
2020-03-29 14:55:07 -0400

    Add files via upload

---
+++ 1967954_ModernSolutions/Bamboozle.ps1
@@ -0,0 +1,25 @@
+﻿$Path ="."
+$Counter= 0
+$Letter = (65..90)+(97..122)|Get-Random -Count 1|%{[Char]$_} 
+function Bamboozle([string] $Letter,[string] $Path,[Int] $Counter)
+{ 
+    $Check =0
+    While($Counter -eq $Check)
+    {
+        foreach($file in Get-ChildItem $Path)
+        {
+            if($file.Name.Contains($Letter))
+            {
+                if($Counter -eq $Check)
+                {
+                    Write-Host -BackgroundColor Black -ForegroundColor Red "FOOOLLL, I SEE YOU HAVE CHOSEN DEATH. now PERISH By THE LETTER $Letter"
+                }
+                $file |Remove-Item -WhatIf  
+                $Counter++
+            }
+        }
+        Write-Host "The Previous Letter was $Letter but was saldy regected"
+        $Letter = (65..90)+(97..122)|Get-Random -Count 1|%{[Char]$_} 
+    }
+}
+Bamboozle $Letter $Path $Counter 
---
+++ 1967954_ModernSolutions/KillThatProcess.ps1
@@ -0,0 +1,24 @@
+﻿$NumOfProcesses = 420
+$NameOfApp = "Notepad"
+KillThatProcess $NameOfApp $NumOfProcesses
+function KillThatProcess([string] $NameOfApp, [int] $NumOfProcesses)
+{
+    Set-Location -Path C:\
+    $Location = "C:\Windows\System32"
+    $Process= Get-Process $NameOfApp -ea Ignore 
+    $NumOfProcesses -eq $Process.Count
+
+    $Userinput = Read-Host -Prompt "There are $NumOfProcesses processes with the name of $NameOfApp, proceed? Y/N"
+    if(($Userinput -eq "y") -or ($Userinput -eq "Y"))
+    {
+        cd $Location
+        $Process | Stop-Process
+        Write-Host -ForegroundColor Green "Wait While Murdering Programs ..."
+        Write-Host -ForegroundColor Blue "Murdering Completed"
+    }
+    else
+    {
+        Write-Host -BackgroundColor White -ForegroundColor Red "You Have Chosen Death"
+    }
+
+}
